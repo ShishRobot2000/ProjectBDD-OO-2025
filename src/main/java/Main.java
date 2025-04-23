70408792f62feced8f39245de6b2afe61d3f5cc3
ShishRobot2000 <72952819+ShishRobot2000@users.noreply.github.com>
2025-04-23 10:36:20 +0200

    Sistemato l'output in maniera più leggibile e aggiunto nel main un output per testare i ToDo legati a una bacheca

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,9 +21,20 @@ public class Main {
                 "colonna 1"
         );
 
+        ToDo task2 = new ToDo(
+                "Prenotare sala riunioni",
+                "2025-04-25",
+                "http://azienda.com/riunione",
+                null,
+                "Prenotare la sala per la riunione del team",
+                "blu",
+                "colonna 2"
+        );
+
         // 4. Associo il ToDo alla bacheca
         List<ToDo> listaToDo = new ArrayList<>();
         listaToDo.add(task1);
+        listaToDo.add(task2);
         lavoro.setToDoList(listaToDo);
 
         // 5. Associo la bacheca all’utente
@@ -38,14 +49,17 @@ public class Main {
 
         List<ToDo> toDoCondivisi = new ArrayList<>();
         toDoCondivisi.add(task1);
+        toDoCondivisi.add(task2);
         pietro.setToDoCondivisi(toDoCondivisi);
 
         // 7. Stampiamo per vedere che funzioni
+        System.out.println("-----------------------------");
         System.out.println("Utente: " + pietro.getUsername());
         System.out.println("Bacheca: " + lavoro.getDescrizione());
         System.out.println("ToDo: " + task1.getTitolo());
         System.out.println("Stato: " + task1.getStato()); // deve stampare NonCompletato
-
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
         System.out.println("Dettagli ToDo:");
         System.out.println("Titolo: " + task1.getTitolo());
         System.out.println("Scadenza: " + task1.getDataDiScadenza());
@@ -55,6 +69,17 @@ public class Main {
         System.out.println("Posizione: " + task1.getPosizione());
         System.out.println("URL: " + task1.getUrl());
         System.out.println("Immagine: " + (task1.getImmagine() != null ? task1.getImmagine() : "Nessuna"));
+        System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
+        System.out.println("\nElenco ToDo nella bacheca:");
+        for (ToDo t : lavoro.getToDoList())
+        {
+            System.out.println("-----------------------------");
+            System.out.println("Titolo: " + t.getTitolo());
+            System.out.println("Stato: " + t.getStato());
+            System.out.println("Scadenza: " + t.getDataDiScadenza());
+            System.out.println("-----------------------------");
+        }
     }
 }
 
